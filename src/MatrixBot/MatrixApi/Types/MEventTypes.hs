b50e350cee43b99bc6d7a5e0e6ae6eaef270a295
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2022-06-28 05:00:57 +0300

    Fix generic JSON parsing for MEventTypes

--- src/MatrixBot/MatrixApi/Types/MEventTypes.hs
+++ src/MatrixBot/MatrixApi/Types/MEventTypes.hs
@@ -25,7 +25,6 @@ module MatrixBot.MatrixApi.Types.MEventTypes
 
 import GHC.TypeLits
 
-import Data.Foldable (foldl')
 import Data.Function (on)
 import Data.List (find)
 import Data.Proxy
@@ -33,8 +32,6 @@ import Data.String (IsString (fromString))
 import Data.Typeable
 import qualified Data.Aeson as Aeson
 import qualified Data.Aeson.Types as Aeson
-
-import Control.Applicative ((<|>), Alternative (empty))
 
 import Servant.API (ToHttpApiData (..))
 
@@ -53,28 +50,12 @@ type family MEventTypeToString (a ∷ MEventType) where
   MEventTypeToString 'MRoomMessageType = "m.room.message"
   MEventTypeToString 'MLoginPasswordType = "m.login.password"
 
-class DescendMEventType (a ∷ MEventType) where descendMEventType ∷ Proxy a → MEventType
-instance DescendMEventType 'MAnnotationType where descendMEventType Proxy = MAnnotationType
-instance DescendMEventType 'MReactionType where descendMEventType Proxy = MReactionType
-instance DescendMEventType 'MRoomMessageType where descendMEventType Proxy = MRoomMessageType
-instance DescendMEventType 'MLoginPasswordType where descendMEventType Proxy = MLoginPasswordType
-
 mEventTypeToString ∷ IsString s ⇒ MEventType → s
 mEventTypeToString = fromString . \case
   MAnnotationType → symbolVal $ Proxy @(MEventTypeToString 'MAnnotationType)
   MReactionType → symbolVal $ Proxy @(MEventTypeToString 'MReactionType)
   MRoomMessageType → symbolVal $ Proxy @(MEventTypeToString 'MRoomMessageType)
   MLoginPasswordType → symbolVal $ Proxy @(MEventTypeToString 'MLoginPasswordType)
-
-instance Aeson.ToJSON MEventType where
-  toJSON = Aeson.String . mEventTypeToString
-
-instance Aeson.FromJSON MEventType where
-  parseJSON ∷ ∀a. (a ~ MEventType) ⇒ Aeson.Value → Aeson.Parser a
-  parseJSON jsonInput
-    = maybe (Aeson.typeMismatch (show . typeRep $ Proxy @a) jsonInput) pure
-    $ find ((jsonInput ==) . Aeson.toJSON) [minBound .. maxBound ∷ a]
-
 
 -- | Limited/restricted subset of "MEventType"
 data MEventTypeOneOf (types ∷ [MEventType]) where
@@ -90,6 +71,25 @@ mEventTypeOneOfToMEventType = \case
   MRoomMessageTypeOneOf → MRoomMessageType
   MLoginPasswordTypeOneOf → MLoginPasswordType
 
+instance (ItemParser t ts to x xs, t ~ 'MAnnotationType) ⇒ ToParser '(to, 'MAnnotationType ': ts) where
+  toParser Proxy = genericToParser (Proxy @'(to, t ': ts)) MAnnotationTypeOneOf
+instance (ItemParser t ts to x xs, t ~ 'MReactionType) ⇒ ToParser '(to, 'MReactionType ': ts) where
+  toParser Proxy = genericToParser (Proxy @'(to, t ': ts)) MReactionTypeOneOf
+instance (ItemParser t ts to x xs, t ~ 'MRoomMessageType) ⇒ ToParser '(to, 'MRoomMessageType ': ts) where
+  toParser Proxy = genericToParser (Proxy @'(to, t ': ts)) MRoomMessageTypeOneOf
+instance (ItemParser t ts to x xs, t ~ 'MLoginPasswordType) ⇒ ToParser '(to, 'MLoginPasswordType ': ts) where
+  toParser Proxy = genericToParser (Proxy @'(to, t ': ts)) MLoginPasswordTypeOneOf
+
+
+instance Aeson.ToJSON MEventType where
+  toJSON = Aeson.String . mEventTypeToString
+
+instance Aeson.FromJSON MEventType where
+  parseJSON ∷ ∀a. (a ~ MEventType) ⇒ Aeson.Value → Aeson.Parser a
+  parseJSON jsonInput
+    = maybe (Aeson.typeMismatch (show . typeRep $ Proxy @a) jsonInput) pure
+    $ find ((jsonInput ==) . Aeson.toJSON) [minBound .. maxBound ∷ a]
+
 
 -- Matching non-empty list (it’s impossible to have any value of an empty list)
 instance Show (MEventTypeOneOf (t ': ts)) where
@@ -104,78 +104,29 @@ instance ToHttpApiData (MEventTypeOneOf (t ': ts)) where
 instance Aeson.ToJSON (MEventTypeOneOf (t ': ts)) where
   toJSON = Aeson.toJSON . mEventTypeOneOfToMEventType
 
--- FIXME For some reason this instance fails with:
---   • Couldn't match type ‘'MLoginPasswordType’ with ‘'MReactionType’
---       arising from a use of ‘myGenericParseJSON’
-{-instance (IsEmpty a ~ 'False, MEventTypesToParsers a (ListDup a)) ⇒ Aeson.FromJSON (MEventTypeOneOf a) where
-  parseJSON jsonInput
-    = foldl' (\acc f → f jsonInput <|> acc) (Aeson.typeMismatch "MEventTypeOneOf" jsonInput)
-    $ mEventTypesToParsers (Proxy @'(a, ListDup a))-}
-instance Aeson.FromJSON (MEventTypeOneOf '[ 'MLoginPasswordType ]) where
-  parseJSON ∷ ∀t. t ~ 'MLoginPasswordType ⇒ Aeson.Value → Aeson.Parser (MEventTypeOneOf '[t])
-  parseJSON j
-    | j == Aeson.toJSON (descendMEventType @t Proxy) = pure MLoginPasswordTypeOneOf
-    | otherwise = empty
-instance Aeson.FromJSON (MEventTypeOneOf '[ 'MRoomMessageType ]) where
-  parseJSON ∷ ∀t. t ~ 'MRoomMessageType ⇒ Aeson.Value → Aeson.Parser (MEventTypeOneOf '[t])
-  parseJSON j
-    | j == Aeson.toJSON (descendMEventType @'MRoomMessageType Proxy) = pure MRoomMessageTypeOneOf
-    | otherwise = empty
-instance Aeson.FromJSON (MEventTypeOneOf '[ 'MAnnotationType ]) where
-  parseJSON ∷ ∀t. t ~ 'MAnnotationType ⇒ Aeson.Value → Aeson.Parser (MEventTypeOneOf '[t])
-  parseJSON j
-    | j == Aeson.toJSON (descendMEventType @'MAnnotationType Proxy) = pure MAnnotationTypeOneOf
-    | otherwise = empty
+instance ToParser '(t ': ts, t ': ts) ⇒ Aeson.FromJSON (MEventTypeOneOf (t ': ts)) where
+  parseJSON = toParser (Proxy @'(t ': ts, t ': ts))
 
 
-{-class MEventTypesToParsers (typesToParse ∷ [MEventType]) (types ∷ [(MEventType, MEventType)]) where
-  mEventTypesToParsers
-    ∷ Proxy '(typesToParse, types)
-    → [Aeson.Value → Aeson.Parser (MEventTypeOneOf typesToParse)]
+-- | Serialize a list of "MEventType"s to a JSON parser
+class ToParser (a ∷ ([MEventType], [MEventType])) where
+  toParser ∷ Proxy a → Aeson.Value → Aeson.Parser (MEventTypeOneOf (Fst a))
 
-instance MEventTypesToParsers (t ': ts) '[] where
-  mEventTypesToParsers Proxy = []
+instance ToParser '(x ': xs, '[]) where
+  toParser Proxy = Aeson.typeMismatch "MEventTypeOneOf"
 
-instance
-  (MEventTypesToParsersItem t ts types s, t ~ 'MAnnotationType)
-  ⇒ MEventTypesToParsers types ('(t, 'MAnnotationType) ': ts)
-  where
-  mEventTypesToParsers Proxy = mEventTypesToParsersGeneric (Proxy @'(t, ts, types, s)) MAnnotationTypeOneOf
+type ItemParser t ts to x xs = (to ~ x ': xs, OneOf t to ~ 'True, ToParser '(to, ts))
 
-instance
-  (MEventTypesToParsersItem t ts types s, t ~ 'MReactionType)
-  ⇒ MEventTypesToParsers types ('(t, 'MReactionType) ': ts)
-  where
-  mEventTypesToParsers Proxy = mEventTypesToParsersGeneric (Proxy @'(t, ts, types, s)) MReactionTypeOneOf
-
-instance
-  (MEventTypesToParsersItem t ts types s, t ~ 'MRoomMessageType)
-  ⇒ MEventTypesToParsers types ('(t, 'MRoomMessageType) ': ts)
-  where
-  mEventTypesToParsers Proxy = mEventTypesToParsersGeneric (Proxy @'(t, ts, types, s)) MRoomMessageTypeOneOf
-
-instance
-  (MEventTypesToParsersItem t ts types s, t ~ 'MLoginPasswordType)
-  ⇒ MEventTypesToParsers types ('(t, 'MLoginPasswordType) ': ts)
-  where
-  mEventTypesToParsers Proxy = mEventTypesToParsersGeneric (Proxy @'(t, ts, types, s)) MLoginPasswordTypeOneOf
-
-type MEventTypesToParsersItem t ts types s =
-  ( t ~ 'MReactionType
-  , s ~ MEventTypeToString t
-  , OneOf t types ~ 'True
-  , IsEmpty types ~ 'False
-  , MEventTypesToParsers types ts
-  )
-
-mEventTypesToParsersGeneric
-  ∷ ∀ t ts types s . MEventTypesToParsersItem t ts types s
-  ⇒ Proxy '(t, ts, types, s)
-  → MEventTypeOneOf types
-  → [Aeson.Value → Aeson.Parser (MEventTypeOneOf types)]
-mEventTypesToParsersGeneric Proxy x
-  = (\j → if j == Aeson.String (fromString . symbolVal $ Proxy @s) then pure x else empty)
-  : mEventTypesToParsers (Proxy @'(types, ts))-}
+genericToParser
+  ∷ ∀ t ts to x xs
+  . ItemParser t ts to x xs
+  ⇒ Proxy '(to, t ': ts)
+  → MEventTypeOneOf to
+  → Aeson.Value
+  → Aeson.Parser (MEventTypeOneOf to)
+genericToParser Proxy x j
+  | j == Aeson.toJSON (mEventTypeOneOfToMEventType x) = pure x
+  | otherwise = toParser (Proxy @'(to, ts)) j
 
 
 -- * Helpers
@@ -185,10 +136,5 @@ type family OneOf (x ∷ k) (xs ∷ [k]) ∷ Bool where
   OneOf x (x ': _) = 'True
   OneOf x (y ': ys) = OneOf x ys
 
-type family IsEmpty (a ∷ [k]) ∷ Bool where
-  IsEmpty '[] = 'True
-  IsEmpty (_ ': _) = 'False
-
-type family ListDup (a ∷ [k]) ∷ [(k, k)] where
-  ListDup '[] = '[]
-  ListDup (x ': xs) = '(x, x) ': ListDup xs
+type family Fst (a ∷ (k1, k2)) ∷ k1 where
+  Fst '(a, _) = a
